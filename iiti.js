dd9b67fd8f843ca0172466baf0025ba042ed35b9
sunilpar <sunilformal42@gmail.com>
2025-06-21 03:34:57 +0000

    writing scrape functions for iiti site with proper format

---
+++ iiti.js
@@ -0,0 +1,11 @@
+import puppeteer from 'puppeteer';
+
+
+export async function scrapeIiti(Iurl) {
+}
+
+export async function scrapeIitiprice(Iurl, lowPrice, highPrice) {
+}
+
+function parse() {
+}
--- index.js
+++ index.js
@@ -1,41 +1,41 @@
-import { scrapeHamrobazaar } from './hamrobazar.js';
-import { scrapeHamrobazaarPrice } from './hamrobazar.js';
-import { scrapeDaraz } from './daraz.js';
-import fs from 'fs';
-
-(async () => {
-  const query = 'fan';
-  console.time('Total time');
-
-  const lowPrice = 100
-  const highPrice = 1000
-  const Durl = `https://www.daraz.com.np/catalog/?q=fan&price=${lowPrice}-${highPrice}`;
-  const Hurl = `https://hamrobazaar.com/search/product?q=${query}`;
-
-
-  const hamroPromise = (async () => {
-    console.time('Hamrobazaar time');
-    const products = await scrapeHamrobazaarPrice(Hurl,lowPrice,highPrice);
-    const output = products.map((item, i) => (
-      `\nSite: ${item.site}\nProduct ${i + 1}\nTitle: ${item.title}\nPrice: ${item.price}\nDescription: ${item.description}\nImage: ${item.img}\nLink: ${item.href}\n`
-    )).join('\n');
-    fs.writeFileSync('hamroBazar.txt', output, 'utf-8');
-    console.timeEnd('Hamrobazaar time');
-  })();
-
-  const darazPromise = (async () => {
-    console.time('Daraz time');
-    const products = await scrapeDaraz(Durl);
-    const output = products.map((item, i) => (
-      `\nSite: ${item.site}\nProduct ${i + 1}\nTitle: ${item.title}\nPrice: ${item.price}\nImage: ${item.img}\nLink: ${item.href}\n`
-    )).join('\n');
-    fs.writeFileSync('daraz.txt', output, 'utf-8');
-    console.timeEnd('Daraz time');
-  })();
-
-  await Promise.all([hamroPromise, darazPromise]);
-
-  console.timeEnd('Total time');
-})();
-
-
+//import { scrapeHamrobazaar } from './hamrobazar.js';
+//import { scrapeHamrobazaarPrice } from './hamrobazar.js';
+//import { scrapeDaraz } from './daraz.js';
+//import fs from 'fs';
+//
+//(async () => {
+//  const query = 'fan';
+//  console.time('Total time');
+//
+//  const lowPrice = 100
+//  const highPrice = 1000
+//  const Durl = `https://www.daraz.com.np/catalog/?q=fan&price=${lowPrice}-${highPrice}`;
+//  const Hurl = `https://hamrobazaar.com/search/product?q=${query}`;
+//
+//
+//  const hamroPromise = (async () => {
+//    console.time('Hamrobazaar time');
+//    const products = await scrapeHamrobazaar(Hurl);
+//    const output = products.map((item, i) => (
+//      `\nSite: ${item.site}\nProduct ${i + 1}\nTitle: ${item.title}\nPrice: ${item.price}\nDescription: ${item.description}\nImage: ${item.img}\nLink: ${item.href}\n`
+//    )).join('\n');
+//    fs.writeFileSync('hamroBazar.txt', output, 'utf-8');
+//    console.timeEnd('Hamrobazaar time');
+//  })();
+//
+//  const darazPromise = (async () => {
+//    console.time('Daraz time');
+//    const products = await scrapeDaraz(Durl);
+//    const output = products.map((item, i) => (
+//      `\nSite: ${item.site}\nProduct ${i + 1}\nTitle: ${item.title}\nPrice: ${item.price}\nImage: ${item.img}\nLink: ${item.href}\n`
+//    )).join('\n');
+//    fs.writeFileSync('daraz.txt', output, 'utf-8');
+//    console.timeEnd('Daraz time');
+//  })();
+//
+//  await Promise.all([hamroPromise, darazPromise]);
+//
+//  console.timeEnd('Total time');
+//})();
+//
+//
