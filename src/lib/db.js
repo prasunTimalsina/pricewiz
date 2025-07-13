9b468727cc696aafd09e621e05f40cad23e9aa4f
sunilpar <sunilformal42@gmail.com>
2025-07-13 04:22:52 +0000

    next js setup with postgre db connected

---
+++ src/lib/db.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
+});
+
+export default pool;
