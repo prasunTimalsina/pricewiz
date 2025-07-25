c8e0c0b896f7ae3c6bed9aadcb252a8aa7267d10
sunilpar <sunilformal42@gmail.com>
2025-07-25 15:35:45 +0000

    Bug: the autometed task seems to be issue of async on schedular itself

--- expressServer/scheduler.js
+++ expressServer/scheduler.js
@@ -5,8 +5,9 @@ const task = cron.schedule(
     "* * * * *",
     async () => {
         try {
-            const timestamp = await getTimestamp()
-            console.log(`✅ Automated run at DB timestamp: ${timestamp}`)
+            //const timestamp = await getTimestamp()
+            //console.log(`✅ Automated run at DB timestamp: ${timestamp}`)
+            console.log(`✅ Automated run at DB timestamp: without async`)
         } catch (err) {
             console.error("❌ Error fetching timestamp:", err.message)
         }
